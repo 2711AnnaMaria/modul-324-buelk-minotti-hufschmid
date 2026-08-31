74b74ed5438cb8fbf196741b87bc2d5d963b0e2e
Luca Minotti <luca.minotti@bbzbl-it.ch>
2026-08-31 15:31:27 +0200

    feat: add editor component with routing back to home component

---
+++ app/src/app/pages/editor/editor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-editor',
+  imports: [RouterLink],
+  templateUrl: './editor.component.html',
+  styleUrl: './editor.component.scss',
+})
+export class EditorComponent {}
