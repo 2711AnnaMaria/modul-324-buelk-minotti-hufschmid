fcb7d2cecf647acf241d31bb85b48d25293f1676
Ramon Hufschmid <ramon.hufschmid@bbzbl-it.ch>
2026-09-21 10:58:22 +0200

    feat: Add Vitest to project (#52)
    * feat: Add Playwright to project (#51)
    
    * feat: Add Vitest to project (#52)
    
    * feat: Add Vitest to project (#52)
    
    ---------
    
    Co-authored-by: Ramon <ramonhufschmid@gmail.com>

--- app/src/app/pages/editor/editor.component.spec.ts
+++ app/src/app/pages/editor/editor.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { EditorComponent } from './editor.component';
 
 describe('EditorComponent', () => {
@@ -8,6 +9,7 @@ describe('EditorComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [EditorComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EditorComponent);
